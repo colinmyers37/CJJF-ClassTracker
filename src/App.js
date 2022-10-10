cfdcbd3cc5c6f8eebc5599a0dfd522cf06ade5e9
colinmyers37 <colinmyers37@gmail.com>
2022-10-10 14:43:09 -0500

    check

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  console.log("hello world");
   return (
     <div className="App">
       <button className="btn btn-danger">Bootstrap working</button>
