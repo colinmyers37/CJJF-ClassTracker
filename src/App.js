ee1769321a9a37fa8ca896cf6b9dad226c9b69cb
colinmyers37 <colinmyers37@gmail.com>
2022-10-10 02:11:14 -0500

    added dependencies for starter project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <button className="btn btn-danger">Bootstrap working</button>
+    </div>
+  );
 }
 
 export default App;
