64852cc838b4b6f3f8c3a4f55fe7ba03d29634e4
colinmyers37 <colinmyers37@gmail.com>
2022-11-11 20:14:41 -0600

    adding add session modal still needs work

--- src/components/user/InsightsPage.js
+++ src/components/user/InsightsPage.js
@@ -1,35 +1,35 @@
-import React from "react";
-import AddSession from "../main/AddSession/AddSession";
-import SessionCard from "../main/SessionCard/SessionCard";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React from 'react';
+import AddSession from '../main/AddSession/AddSession';
+import SessionCard from '../main/SessionCard/SessionCard';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const InsightsPage = () => {
   return (
     <div
       style={{
-        backgroundColor: "#A11F22",
-        height: "100vh",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
+        backgroundColor: '#A11F22',
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
       }}
     >
-      <h2 style={{ fontSize: "30px", color: "white" }}>Insights</h2>
+      <h2 style={{ fontSize: '30px', color: 'white' }}>Insights</h2>
       <div
         style={{
-          backgroundColor: "white",
-          width: "95vw",
-          height: "95vh",
-          borderRadius: "10px",
+          backgroundColor: 'white',
+          width: '95vw',
+          height: '95vh',
+          borderRadius: '10px',
         }}
       >
         <div
           style={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
           }}
         >
           <AddSession />
